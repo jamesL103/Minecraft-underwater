31d101bda927590508c8eefbdaa13af588675f9d
jamesL103 <jameskl7337@gmail.com>
2025-01-20 13:45:11 -0500

    Added title for equipment screen

--- src/client/java/james/underwater/UnderwaterDataGenerator.java
+++ src/client/java/james/underwater/UnderwaterDataGenerator.java
@@ -1,11 +1,14 @@
 package james.underwater;
 
+import james.underwater.providers.UnderwaterEnglishLanguageProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class UnderwaterDataGenerator implements DataGeneratorEntrypoint {
+
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(UnderwaterEnglishLanguageProvider::new);
 	}
 }
---
+++ src/client/java/james/underwater/providers/UnderwaterEnglishLanguageProvider.java
@@ -0,0 +1,34 @@
+package james.underwater.providers;
+
+import james.underwater.EquipmentScreenHandlerFactory;
+import james.underwater.Underwater;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableTextContent;
+
+import java.util.concurrent.CompletableFuture;
+
+public class UnderwaterEnglishLanguageProvider extends FabricLanguageProvider {
+
+    public UnderwaterEnglishLanguageProvider(FabricDataOutput dataGenerator, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        // Specifying en_us is optional, by default it is en_us.
+        super(dataGenerator, "en_us", registryLookup);
+    }
+
+    @Override
+    public void generateTranslations(RegistryWrapper.WrapperLookup wrapperLookup, TranslationBuilder translationBuilder) {
+        addText(translationBuilder, EquipmentScreenHandlerFactory.TITLE, "Equipment");
+    }
+
+    //helper to make Identifier out of Text and add it to datagen
+    private static void addText(TranslationBuilder builder, Text text, String value) {
+        if (text.getContent() instanceof TranslatableTextContent translatableTextContent) {
+            builder.add(translatableTextContent.getKey(), value);
+        } else {
+            Underwater.LOGGER.warn("Failed to add translation key for text: {}", text.getString());
+        }
+    }
+
+}
